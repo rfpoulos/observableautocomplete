cb64c8610ed513f6c7bac22ad12cfee1a3e88d03
rfpoulos <rfpoulos@outlook.com>
2018-07-18 10:07:31 -0400

    uses rxjs 6

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
+const { from, fromEvent } = rxjs;
+const { map, filter, switchMap, debounceTime, distinctUntilChanged, startWith } = rxjs.operators;
 
 let searchBox = document.getElementById('search');
 let results = document.getElementById('results');
 
-var searchGithub = (term) =>
+let searchGithub = (term) =>
     fetch(`https://api.github.com/search/users?q=${term}`)
     .then(data => data.json());
 
-let keyup = Rx.Observable.fromEvent(searchBox, 'keyup')
-    .debounce(500)
-    .map(e => e.target.value)
-    .filter(value => value.length > 2)
-    .distinctUntilChanged()
-    .flatMap(value => Rx.Observable.fromPromise(searchGithub(value)))
-    .subscribe(data =>  {
+let input$ = fromEvent(searchBox, 'input')
+    .pipe(
+        debounceTime(250),
+        map(e => e.target.value),
+        distinctUntilChanged(),
+        switchMap(value => value ? searchGithub(value) : Promise.resolve({items: []}))
+    )
+    
+input$.subscribe(data =>  {
+        console.log(data)
         results.innerHTML = '';
         data.items.map(element => {
             let newResult = document.createElement('li');
